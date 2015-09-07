3d39ac95ab62dd53403f6b034d5fe82523e19a13
N. Taylor Mullen <nimullen@microsoft.com>
2015-09-06 22:12:17 -0700

    Finished EmailTagHelper.cs.

---
+++ src/TagHelperDemo/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperDemo.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var domain = MailTo + "@example.com";
+            output.Content.SetContent(domain);
+            output.Attributes["href"] = "mailto:" + domain;
+            output.PreElement.SetContent(new HtmlString($"<strong>{MailTo}:</strong> "));
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
