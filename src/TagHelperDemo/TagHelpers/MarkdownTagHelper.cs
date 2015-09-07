db156b7fa5df464dc2cc186038f47509dc829480
N. Taylor Mullen <nimullen@microsoft.com>
2015-09-06 22:57:29 -0700

    Finish MarkdownTagHelper.cs.

---
+++ src/TagHelperDemo/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using MarkdownSharp;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperDemo.TagHelpers
+{
+    [TargetElement("markdown")]
+    [TargetElement(Attributes = "markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        private static readonly Markdown MarkdownParser = new Markdown();
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await context.GetChildContentAsync();
+            var parsedContent = MarkdownParser.Transform(childContent.GetContent());
+
+            output.Content.SetContent(new HtmlString(parsedContent));
+        }
+    }
+}
